51d362f1e65caa64ba4befd7235f27ed83c88349
Darko Malogajski <darko.malogajski@gmail.com>
2024-02-27 03:59:17 +0100

    feat: create item

--- app/Livewire/Item/Index.php
+++ app/Livewire/Item/Index.php
@@ -3,14 +3,36 @@
 namespace App\Livewire\Item;
 
 use App\Models\Item;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $name;
+
     public function render()
     {
-        $items = Item::paginate(5);
+        $items = Item::orderBy('id', 'desc')->paginate(10);
 
         return view('livewire.item.index', compact('items'))->layout('layouts.app');
+    }
+
+    public function addItem()
+    {
+        $data = [
+            'team_id'    => auth()->user()->team_id,
+            'name'       => $this->name,
+            'user_id'    => auth()->user()->id,
+            'created_at' => Carbon::now(),
+        ];
+
+        $isItemExists = Item::where('team_id', auth()->user()->team_id)
+            ->where('name', $this->name)
+            ->exists();
+
+        if (!$isItemExists) {
+            Item::create($data);
+            $this->dispatch('$refresh');
+        }
     }
 }
--- resources/views/livewire/item/index.blade.php
+++ resources/views/livewire/item/index.blade.php
@@ -1,8 +1,17 @@
 <div class="m-6 p-6 bg-white dark:bg-gray-800 dark:text-gray-300 rounded-xl">
     <p class="text-lg font-semibold mb-6">List of Items</p>
 
-    <div class="w-1/3">
-        <table class="w-full">
+    <div x-data="{ createItem:false }" class="mt-2 mb-4 text-gray-700 dark:text-gray-300">
+        <x-primary-button @click="createItem = !createItem">Add Item</x-primary-button>
+        <div x-show="createItem" class="mt-6">
+            <span>Item name:</span>
+            <input type="text" class="rounded mr-2 bg-gray-50 dark:bg-gray-800" wire:model="name">
+            <x-primary-button wire:click="addItem" @click="createItem = false">Add</x-primary-button>
+        </div>
+    </div>
+
+    <div class="w-full">
+        <table class="w-full text-sm">
             <thead>
             <tr class="bg-gray-200 dark:bg-gray-900">
                 <th class="p-2">#</th>
@@ -13,7 +22,7 @@
             <tbody>
             @forelse($items as $item)
                 <tr class="odd:bg-gray-50 dark:odd:bg-gray-700">
-                    <td class="px-2 py-1 text-center">{{ $item->id }}</td>
+                    <td class="px-2 py-2 text-center">{{ $item->id }}</td>
                     <td class="text-center">{{ $item->name }}</td>
                     <td class="text-center">{{ $item->user->name }}</td>
                 </tr>
