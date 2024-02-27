f610f4df10927c7d04a31d69d2d272e96fb2aa16
Darko Malogajski <darko.malogajski@gmail.com>
2024-02-27 02:33:26 +0100

    change: updated shopping list index

--- resources/views/livewire/shopping-list-item/create.blade.php
+++ resources/views/livewire/shopping-list-item/create.blade.php
@@ -1,12 +1,20 @@
-<div x-data="{ showItems:false }">
+<div x-data="{ showItems: false }">
     <div class="m-6 p-6 bg-white text-gray-700 dark:text-gray-300 rounded-xl shadow-xl dark:bg-gray-800">
-        <div class="my-6">
-            <p class="text-sm">List of items:</p>
-            <p class="font-semibold text-xl">{{ $shoppingList->name ?? 'n-a' }}</p>
+        <!-- Back Button at the top -->
+        <div class="flex justify-center">
+            <x-secondary-button onclick="history.back()">
+                <i class="fa fa-arrow-alt-circle-left mr-1"></i> Back
+            </x-secondary-button>
+        </div>
+
+        <div class="flex justify-between items-center mb-4">
+            <div>
+                <p class="text-sm">List of items:</p>
+                <p class="font-semibold text-xl">{{ $shoppingList->name ?? 'n-a' }}</p>
+            </div>
         </div>
         <div class="w-full flex justify-center my-2 mb-2">
-            <x-primary-button class=""
-                              @click="showItems = !showItems"
+            <x-primary-button @click="showItems = !showItems"
                               x-text="showItems ? 'Hide items' : 'Show items'"
                               class="transition duration-200 ease-in-out transform hover:scale-105"/>
         </div>
@@ -17,8 +25,7 @@
              x-transition:enter-end="opacity-100 transform scale-100"
              x-transition:leave="transition ease-in duration-300"
              x-transition:leave-start="opacity-100 transform scale-100"
-             x-transition:leave-end="opacity-0 scale-90"
-        >
+             x-transition:leave-end="opacity-0 scale-90">
             @foreach($items as $item)
                 @php
                     $isInList = \App\Models\ShoppingListItem::where('shopping_list_id', $shopping_list_id)->where('item_id', $item->id)->exists();
--- resources/views/livewire/shopping-list/index.blade.php
+++ resources/views/livewire/shopping-list/index.blade.php
@@ -1,19 +1,19 @@
 <div class="m-6 p-6 bg-white rounded-xl shadow-xl dark:bg-gray-800">
 
-    <div class="flex justify-end mb-6">
-        <x-primary-button wire:click="newShoppingList">New Shopping List</x-primary-button>
+    <div class="flex justify-end mb-8">
+        <x-primary-button class="transition duration-200 ease-in-out transform hover:scale-105 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" wire:click="newShoppingList">New Shopping List</x-primary-button>
     </div>
 
-    <ul class="flex flex-wrap gap-x-2 gap-y-2">
+    <ul class="flex flex-col gap-y-3">
         @forelse($shoppingLists as $shoppingList)
-            <li>
-                <x-secondary-button wire:click="goToItems({{$shoppingList->id}})">{{ $shoppingList->name }}</x-secondary-button>
+            <li class="">
+                <x-secondary-button class="w-full text-center flex justify-center transition duration-200 ease-in-out transform hover:scale-105 bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-3 px-4 rounded" wire:click="goToItems({{$shoppingList->id}})">{{ $shoppingList->name }}</x-secondary-button>
             </li>
         @empty
-            <p>No Data</p>
+            <div class="text-center py-4">
+                <p class="text-gray-700 dark:text-gray-400">No Data</p>
+            </div>
         @endforelse
     </ul>
-
-
 
 </div>
