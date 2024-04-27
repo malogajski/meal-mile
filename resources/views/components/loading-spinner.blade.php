c1c2832c6ae5bbffe1890faf4794584939b9d3f0
Darko Malogajski <darko.malogajski@gmail.com>
2024-04-27 16:47:11 +0200

    feat: loading-spinner component

---
+++ resources/views/components/loading-spinner.blade.php
@@ -0,0 +1,9 @@
+<div wire:loading class="z-50">
+    <div wire:loading.class="opacity-50">
+        <!-- grayed-out background overlay -->
+        <div class="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50">
+            <!-- spinner -->
+            <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/shopping-list/index.blade.php
+++ resources/views/livewire/shopping-list/index.blade.php
@@ -1,6 +1,6 @@
 <div class="m-6 p-6 bg-white rounded-xl shadow-xl dark:bg-gray-800">
-    <div wire:loading>Loading...</div>
-{{--    <x-danger-button wire:click="sendTestEmail">Send Test Email</x-danger-button>--}}
+    <x-loading-spinner wire:loading/>
+
     <div class="flex justify-end mb-8">
         <x-primary-button class="transition duration-200 ease-in-out transform hover:scale-105 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" wire:click="newShoppingList">
             <i class="fa fa-plus mr-1"></i>
