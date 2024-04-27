f1b08f9918b8befd713692223aa02c50b6d884dc
Darko Malogajski <darko.malogajski@gmail.com>
2024-04-27 23:06:40 +0200

    feat: registration options, button styles; fix: new registration team code

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,16 +12,20 @@ use Laravel\Socialite\Facades\Socialite;
 
 class ProviderController extends Controller
 {
-    public function redirectSocial($provider)
+    public function redirectSocial($provider, Request $request)
     {
+        $request->session()->put('team_code', $request->input('team_code'));
+
         return Socialite::driver($provider)->redirect();
     }
 
     public function callbackSocial($provider)
     {
+        $team_code = decrypt(session('team_code'));
+
         $user = Socialite::driver($provider)->user();
 
-        $success = $this->signOrRegisterWithProvider($user, $provider);
+        $success = $this->signOrRegisterWithProvider($user, $provider, $team_code);
 
         if ($success) {
             return redirect()->route('dashboard');
@@ -29,16 +34,27 @@ class ProviderController extends Controller
         }
     }
 
-    public function signOrRegisterWithProvider($socialUser, $provider)
+    public function signOrRegisterWithProvider($socialUser, $provider, $team_code)
     {
+        // Log the socialite user data
         Log::info('[SOCIALITE ACCESS PAYLOAD][' . $provider . ']: ' . json_encode($socialUser));
 
-
-
-
         try {
+            // Find the user by provider ID or email
             $user = User::where($provider . '_id', $socialUser->id)->orWhere('email', $socialUser->email)->first();
 
+            // Find the team based on team_code
+            if (!empty($team_code)) {
+                $team = Team::where('team_code', $team_code)->first();
+            }
+
+            // If team exists and user doesn't have team_id, assign team to user
+            if (!empty($team) && empty($user->team_id)) {
+                $user->team_id = $team->id;
+                $user->save();
+            }
+
+            // Create or update the user
             if (empty($user)) {
                 $user = User::updateOrCreate([
                     $provider . '_id' => $socialUser->id,
@@ -47,12 +63,14 @@ class ProviderController extends Controller
                     'email'                      => $socialUser->email,
                     $provider . '_token'         => $socialUser->token,
                     $provider . '_refresh_token' => $socialUser->refreshToken ?? null,
+                    'team_id'                   => !empty($team) ? $team->id : null,
                 ]);
             } else {
                 $user->update([
                     $provider . '_id'            => $socialUser->id,
                     $provider . '_token'         => $socialUser->token,
                     $provider . '_refresh_token' => $socialUser->refreshToken ?? null,
+                    'team_id'                   => !empty($team) ? $team->id : null,
                 ]);
             }
 
@@ -67,4 +85,5 @@ class ProviderController extends Controller
 
         return false;
     }
+
 }
--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -22,13 +22,14 @@ state([
 ]);
 
 rules([
-    'name'     => ['required', 'string', 'max:255'],
-    'email'    => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-    'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
-    'team_code'  => ['nullable', 'exists:teams,team_code'],
+    'name'      => ['required', 'string', 'max:255'],
+    'email'     => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+    'password'  => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+    'team_code' => ['nullable', 'exists:teams,team_code'],
 ]);
 
 $register = function () {
+
     $validated = $this->validate();
 
     $validated['password'] = Hash::make($validated['password']);
@@ -60,13 +61,36 @@ $register = function () {
 
 ?>
 
-<div>
-    <form wire:submit="register">
-        <!-- Team -->
-        <div class="mt-4">
-            <x-input-label for="team_id" :value="__('Team Code (Optional)')"/>
-            <x-text-input wire:model="team_code" id="team_id" class="block mt-1 w-full" type="text" name="team_id"/>
-            <x-input-error :messages="$errors->get('team_code')" class="mt-2"/>
+<div x-data="{ showTeam: false, showForm: false, showOptions: true, teamCode: '' }">
+    {{-- Options --}}
+    <div class="flex flex-col w-full" x-show="showOptions">
+        <div class="flex justify-between mb-4 space-x-2">
+            <button class="btn-lg btn-default" @click="showTeam = true; showOptions = false">Join the team <i class="fa-solid fa-people-group ml-1"></i></button>
+            <button class="btn-lg btn-purple" @click="showForm = true; showOptions = false">New registration <i class="fa-solid fa-user-plus ml-1"></i></button>
+        </div>
+
+        <a class="text-sm hover:underline text-center" href="{{ route('login') }}">Back to login</a>
+    </div>
+
+    <div class="flex justify-end my-2" x-show="!showOptions" x-cloak>
+        <span class="btn btn-alternative" @click="showForm ? showTeam=true : (showOptions = true, showTeam=false, teamCode = ''); showForm = false; ">
+            <i class="fa-solid fa-circle-left mr-1"></i>Back
+        </span>
+    </div>
+
+    {{-- Team code --}}
+    <div x-show="showTeam" class="flex flex-col w-full">
+        <input type="text" x-model="teamCode" placeholder="Enter Team Code" x-bind:maxlength="16" x-on:input="teamCode.length >= 4 ? $refs.continueButton.removeAttribute('disabled') : $refs.continueButton.setAttribute('disabled', true)">
+        <button x-ref="continueButton" class="btn btn-green mt-2" @click="showForm = true; showTeam = false; $wire.set('team_code', teamCode)" :disabled="teamCode.length < 4 || teamCode.length > 16">Continue</button>
+    </div>
+
+    <form x-show="showForm" x-cloak wire:submit="register">
+
+        <p>TEAM: {{ $team_code ?? '' }}</p>
+        <div>
+            @error('team_code')
+            <span class="text-sm text-red-500">{{ $message }}</span>
+            @enderror
         </div>
         <!-- Name -->
         <div>
@@ -117,56 +141,57 @@ $register = function () {
         </div>
     </form>
 
-    <div class="relative mt-10">
-        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-            <div class="w-full border-t border-gray-200"></div>
+    {{-- Continue with social --}}
+    <div x-show="showForm" x-cloak>
+        <div class="relative mt-10">
+            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                <div class="w-full border-t border-gray-200"></div>
+            </div>
+            <div class="relative flex justify-center text-sm font-medium leading-6">
+                <span class="bg-white px-6 text-gray-900">Or continue with</span>
+            </div>
         </div>
-        <div class="relative flex justify-center text-sm font-medium leading-6">
-            <span class="bg-white px-6 text-gray-900">Or continue with</span>
+        <div class="mt-6 flex w-full items-center justify-between">
+            @php
+                $args = '';
+                if(!empty($team_code)) {
+                    $encrypted = encrypt($team_code);
+                    $args = "?team_code=$encrypted";
+                }
+            @endphp
+                <!-- Google -->
+            <a href="/auth/google/redirect{{$args}}" data-twe-ripple-init data-twe-ripple-color="light"
+               class="mb-2 inline-block rounded bg-[#ea4335] px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-white hover:text-[#ea4335] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+            <span class="[&>svg]:h-4 [&>svg]:w-4">
+              @include('svg.google-icon')
+            </span>
+            </a>
+
+            <!-- Facebook -->
+            <a href="/auth/facebook/redirect{{$args}}" data-twe-ripple-init data-twe-ripple-color="light"
+               class="mb-2 inline-block rounded bg-[#1877f2] px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-white hover:text-[#1877f2] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+            <span class="[&>svg]:h-4 [&>svg]:w-4">
+              @include('svg.facebook-icon')
+            </span>
+            </a>
+
+            <!-- Github -->
+            <a href="/auth/github/redirect{{$args}}" data-twe-ripple-init data-twe-ripple-color="light"
+               class="mb-2 inline-block rounded bg-[#333] hover:bg-white px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white hover:text-black shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+            <span class="[&>svg]:h-4 [&>svg]:w-4">
+                @include('svg.github-icon')
+            </span>
+            </a>
+
+            <!-- X -->
+            <a href="/auth/twitter/redirect{{$args}}" data-twe-ripple-init data-twe-ripple-color="light"
+               class="mb-2 inline-block rounded bg-black px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg hover:bg-white hover:text-black focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+            <span class="[&>svg]:h-4 [&>svg]:w-4">
+              @include('svg.twitter-icon')
+            </span>
+            </a>
+
         </div>
     </div>
-    <div class="mt-6 flex w-full items-center justify-between">
-        <!-- Google -->
-        <a href="/auth/google/redirect" data-twe-ripple-init data-twe-ripple-color="light"
-           class="mb-2 inline-block rounded bg-[#ea4335] px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-white hover:text-[#ea4335] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-    <span class="[&>svg]:h-4 [&>svg]:w-4">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 488 512">
-        <path
-            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/>
-      </svg>
-    </span>
-        </a>
 
-        <!-- Facebook -->
-        <a href="/auth/facebook/redirect" data-twe-ripple-init data-twe-ripple-color="light"
-           class="mb-2 inline-block rounded bg-[#1877f2] px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-white hover:text-[#1877f2] hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-    <span class="[&>svg]:h-4 [&>svg]:w-4">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 320 512">
-        <path
-            d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/>
-      </svg>
-    </span>
-        </a>
-
-        <!-- Github -->
-        <a href="/auth/github/redirect" data-twe-ripple-init data-twe-ripple-color="light"
-           class="mb-2 inline-block rounded bg-[#333] hover:bg-white px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white hover:text-black shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-    <span class="[&>svg]:h-4 [&>svg]:w-4">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 496 512">
-        <path
-            d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-      </svg>
-    </span>
-        </a>
-
-        <!-- X -->
-        <a href="/auth/twitter/redirect" data-twe-ripple-init data-twe-ripple-color="light"
-           class="mb-2 inline-block rounded bg-black px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg hover:bg-white hover:text-black focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-    <span class="[&>svg]:h-4 [&>svg]:w-4">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 512 512">
-        <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/>
-      </svg>
-    </span>
-        </a>
-    </div>
 </div>
